39f45c8bb3f2b763411103190b273eea58c97bca
codertinie <martin.munyao@moringaschool.com>
2022-10-20 15:43:02 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+  #setter method
+  def name=(dog_name)
+    #variable to hold the value
+    @this_dogs_name = dog_name
+  end
 
+  #getter method
+  def name
+    #call the variable to retrieve the value set
+    @this_dogs_name
+  end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie
+
+puts "the dogs name is: #{lassie.name}" # this give an error message bcz this_dogs_name is a local variable
